fa1879ba21e160d9d6821399145a362ca7223c85
qavams <cqs1379@gmail.com>
2025-07-17 12:24:36 +0330

    V6-3

--- Cal/sw.js
+++ Cal/sw.js
@@ -1,6 +1,6 @@
 // sw.js (نسخه اصلاح شده با آیکن‌ها)
 
-const CACHE_NAME = 'persian-calendar-cache-v2';
+const CACHE_NAME = 'persian-calendar-cache-v3';
 const urlsToCache = [
   '/', 
   'calander2.html',
